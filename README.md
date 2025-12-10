7eb23cf1875e8704b42256dab9b6e87ccab8f9c6
Daniel Rødskog <danielmero@gmail.com>
2025-12-11 00:28:02 +0100

    Deploy to Netlify

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Advent of Code 2025
 
-Hey.
+Hey. https://aoc25.netlify.app/ :)
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -5,5 +5,29 @@ export const Route = createFileRoute('/')({
 });
 
 function RouteComponent() {
-  return <div>Hello there!</div>;
+  return (
+    <div className="flex flex-col gap-5">
+      <h2 className="text-2xl">Hello there!</h2>
+      <p>
+        In case you are confused – these pages are solutions to this year's{' '}
+        <a
+          className="text-red-800 hover:underline"
+          href="https://adventofcode.com"
+        >
+          Advent of Code
+        </a>{' '}
+        :)
+      </p>
+      <p>
+        This page's marvellous source is available code over at{' '}
+        <a
+          className="text-red-800 hover:underline"
+          href="https://github.com/daniero/aoc25"
+        >
+          GitHub
+        </a>
+        !
+      </p>
+    </div>
+  );
 }
