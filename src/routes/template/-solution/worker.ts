8bb2dfa94d12c08e8274648a1f283c1271e08bfd
Daniel Rødskog <danielmero@gmail.com>
2025-12-20 16:57:44 +0100

    Stretch

--- src/routes/template/-solution/worker.ts
+++ src/routes/template/-solution/worker.ts
@@ -25,13 +25,18 @@ function loop(time: number) {
   requestAnimationFrame(loop);
 
   // ctx.clearRect(0, 0, width, height);
-  const hue = (time / 30) % 360;
-  const s = (75 + time / 1000) % 100;
-  const l = Math.sin(time / 1000) * 50 + 50;
+  const hue = 217 + ((time / 22 + Math.random() * 12) % 360);
+  const s = (60 + 40 * Math.sin(time / 1000)) % 100;
+  const l = 50 + Math.sin(time / 443) * 50;
   ctx.fillStyle = `hsl(${String(hue)}, ${String(s)}%, ${String(l)}%)`;
 
-  const w = (Math.random() * width) / 3;
-  const h = (Math.random() * height) / 3;
+  const max = 7.5;
+  const period = 7500;
+  const t = time % period;
+  const stretch = max / 2 + (max / 2) * Math.sin((t * Math.PI * 2) / period);
+  const w = (Math.random() * width) / stretch;
+  const h = (Math.random() * height) / (max - stretch);
+
   ctx.fillRect(
     Math.random() * (width + w / 2) - w / 2,
     Math.random() * (height + h / 2) - h / 2,
